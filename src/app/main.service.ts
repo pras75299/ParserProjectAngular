6caa4d0fde4edb7262166a6681900e12c4cebccd
pras75299 <singh.prashantking@gmail.com>
2019-01-10 01:58:10 +0530

    added main service file

---
+++ src/app/main.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+
+export class GetOutputFileService {
+  down() {
+    this.downloadFile('output_user_story.txt',document.getElementById('textval').innerText);
+  }
+  downloadFile = function(filename, data) {
+
+    try {
+        // Try using msSaveBlob to save text file
+        const blob = new Blob([data], {type: 'text/plain'});
+        if (navigator.msSaveBlob) {
+            navigator.msSaveBlob(blob, filename);
+        }
+    } catch (ex) {
+        console.log('saveBlob method failed with the following exception:');
+        console.log(ex);
+    }
+  };
+}
