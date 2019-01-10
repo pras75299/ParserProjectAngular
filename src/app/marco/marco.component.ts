f93c7202fac0a5754b977ad7cdfec219326ead3f
pras75299 <singh.prashantking@gmail.com>
2019-01-10 17:49:07 +0530

    update files for project

--- src/app/marco/marco.component.ts
+++ src/app/marco/marco.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
+import { EventEmitter } from 'events';
 @Component({
   selector: 'app-marco',
   templateUrl: './marco.component.html',
   styleUrls: ['./marco.component.scss']
 })
 export class MarcoComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
+      
   }
+  
+  playMarco(number) {
+    let value='';
+    for(let i=1; i<=number;i++){
+      if(i%4==0 && i%7==0) 
+      value+='marcopolo'
+      else if(i%4==0)  
+        value+='marco'
+      else if(i%7==0) 
+        value+='polo'
+      else
+        value+=i;
+      value+=',';
+      }
+      value = value.substring(0,value.length-1);
+      console.log(value); 
+    return value; 
+    } 
 
+    
+
+    
 }
